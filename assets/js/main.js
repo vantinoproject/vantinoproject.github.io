68f0c358218b2d1c37e347b4ff3dfc95229a5996
AnnaRadchenko <i_believe_i_can_fly_@ukr.net>
2016-06-08 09:37:20 +0300

    vantino project

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+;(function(){
+
+			// Menu settings
+			$('#menuToggle, .menu-close').on('click', function(){
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open');
+			});
+
+
+})(jQuery)
+
+/* ----------------------------------------------------------- */
+	/*  6. SCROLL TOP BUTTON
+	/* ----------------------------------------------------------- */
+
+	//Check to see if the window is top if not then display button
+
+	  jQuery(window).scroll(function(){
+	    if ($(this).scrollTop() > 300) {
+	      $('.scrollToTop').fadeIn();
+	    } else {
+	      $('.scrollToTop').fadeOut();
+	    }
+	  });
+	   
+	  //Click event to scroll to top
+
+	  jQuery('.scrollToTop').click(function(){
+	    $('html, body').animate({scrollTop : 0},800);
+	    return false;
+	  });
